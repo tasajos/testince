340b17db58f58c0896a0074bf37b33b942feb447
Carlos.Azcarraga <tasajos@gmail.com>
2022-10-26 15:29:26 -0400

    test

---
+++ src/FeedingSchedule.java
@@ -0,0 +1,18 @@
+public class FeedingSchedule {
+    public static void main(String[] args) {
+        boolean KeepGoing = true;
+        int count = 0;
+        int x = 3;
+        while (count++ <3){
+            int y = (1 +2 * count) %3;
+            switch (y){
+                default:
+                case 0: x-= 1; break;
+                case 1: x+=5;
+            }
+
+        }
+        System.out.println(x);
+
+    }
+}
